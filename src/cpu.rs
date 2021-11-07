b7422bdb34841266f9bc2759545fb41163414978
Jesse Wyatt <wyattjrw@gmail.com>
2021-11-07 18:15:49 +0800

    Added modular addressing modes and added hashmap for opcode dispatch/metadata.
    Not very happy with this, it's mostly taken from the bugzmanov book.
    When all ops are implemented it can move to a static function table.
    Possibly room here to break down into microops, but can't figure out the structure yet.

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,8 +3,23 @@ mod reg;
 
 use crate::memory::MemoryMap;
 use crate::cpu::reg::RegisterSet;
+use crate::cpu::op::Opcode;
 
 use std::ops::Range;
+
+#[derive(Debug)]
+pub enum AddressingMode {
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    IndirectX,
+    IndirectY,
+    None
+}
 
 /// The NES CPU - Ricoh 2A03 (Modified MOS 6502)
 /// #[derive(default)]
@@ -46,25 +61,80 @@ impl CPU {
         self.mem.load(addr, data);
     }
 
+    fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
+        use AddressingMode::*;
+        match mode {
+            Immediate => self.reg.pc,
+            ZeroPage => self.mem.read_u8(self.reg.pc) as u16,
+            Absolute => self.mem.read_u16(self.reg.pc),
+            ZeroPageX => {
+                self.mem
+                    .read_u8(self.reg.pc)
+                    .wrapping_add(self.reg.x) as u16
+            },
+            ZeroPageY => {
+                self.mem
+                    .read_u8(self.reg.pc)
+                    .wrapping_add(self.reg.y) as u16
+            },
+            AbsoluteX => {
+                self.mem
+                    .read_u16(self.reg.pc)
+                    .wrapping_add(self.reg.x as u16)
+            }
+            AbsoluteY => {
+                self.mem
+                    .read_u16(self.reg.pc)
+                    .wrapping_add(self.reg.y as u16)
+            },
+            IndirectX => {
+                let ptr = self.mem
+                    .read_u8(self.reg.pc)
+                    .wrapping_add(self.reg.x);
+
+                u16::from_le_bytes([self.mem.read_u8(ptr as u16), self.mem.read_u8(ptr.wrapping_add(1) as u16)])
+            }
+            IndirectY => {
+                let ptr = self.mem
+                    .read_u8(self.reg.pc);
+
+                u16::from_le_bytes([self.mem.read_u8(ptr as u16), self.mem.read_u8(ptr.wrapping_add(1) as u16)])
+                    .wrapping_add(self.reg.y as u16)
+            },
+            None => panic!("Addressing Mode Failure: AddressingMode::None"),
+        }
+    }
+
+    pub fn step(&mut self, code: u8) {
+        self.reg.pc += 1;
+        let opcode = op::CPU_OPCODES_MAP.get(&code).expect(&format!("ERROR: Opcode {:x} unimplemented", code));
+
+        match code {
+            // LDA
+            0xA9 | 0xA5 | 0xB5 | 0xAD | 0xBD | 0xB9 | 0xA1 | 0xB1 => self.lda(opcode),
+            // STA
+            0x85 | 0x95 | 0x8D | 0x9D | 0x99 | 0x81 | 0x91 => self.sta(opcode),
+            // TAX
+            0xAA => self.tax(opcode),
+            // INX
+            0xE8 => self.inx(opcode),
+            // CPY
+            0xC0 => self.cpy(opcode),
+            _ => todo!()
+        }
+    }
+
     /// Continuously run program from current location until BRK
     pub fn run(&mut self) {
-
         loop {
-            let opcode = self.mem.read_u8(self.reg.pc);
-            self.reg.pc += 1;
-
-            match opcode {
+            match self.mem.read_u8(self.reg.pc) {
                 op::BRK => return,
-                op::LDA => self.lda(opcode),
-                op::TAX => self.tax(opcode),
-                op::INX => self.inx(opcode),
-                op::CPY => self.cpy(opcode),
-                _ => unimplemented!()
+                opcode => self.step(opcode),
             }
         }
     }
 
-    fn lda(&mut self, code: u8) {
+    fn lda(&mut self, code: &Opcode) {
         let param = self.mem.read_u8(self.reg.pc);
         self.reg.pc += 1;
         self.reg.a = param;
@@ -72,18 +142,22 @@ impl CPU {
         self.update_zero_and_negative_flags(self.reg.a)
     }
 
-    fn tax(&mut self, code: u8) {
+    fn sta(&mut self, code: &Opcode){
+        todo!();
+    }
+
+    fn tax(&mut self, code: &Opcode) {
         self.reg.x = self.reg.a;
 
         self.update_zero_and_negative_flags(self.reg.x);
     }
 
-    fn inx(&mut self, code: u8) {
+    fn inx(&mut self, code: &Opcode) {
         // TODO: this sucks, look into Wrapping<u8>
         self.reg.x = self.reg.x.wrapping_add(1);
     }
 
-    fn cpy(&mut self, code: u8) {
+    fn cpy(&mut self, code: &Opcode) {
         todo!();
     }
 
@@ -101,16 +175,17 @@ impl CPU {
         }
     }
 
-    fn reset_state(&mut self) {
-        self.reg = RegisterSet::default();
-    }
-
+    // fn reset_state(&mut self) {
+    //     self.reg = RegisterSet::default();
+    // }
+    
+    /// Reset register state and initialise program counter to value at 0xFFFC
     fn interrupt_reset(&mut self) {
         self.reg.reset();
-        self.reg.pc = CPU::PRG_START_ADDR;
+        self.reg.pc = self.mem.read_u16(CPU::PRG_START_ADDR);
     }
 
-    fn load_program(&mut self, program: &[u8]) {
+    pub fn load_program(&mut self, program: &[u8]) {
         self.mem.load(CPU::PRG_ROM_ADDR_MIN, program);
         self.mem.write_u16(0xFFFC, CPU::PRG_ROM_ADDR_MIN);
     }
@@ -143,20 +218,30 @@ mod tests {
     #[test]
     fn test_load_transfer_increment() {
         let mut cpu = CPU::new();
-        cpu.load(0, &[LDA, 0xc0, TAX, INX, BRK]);
+        cpu.load(0, &[LDA, 0xC0, TAX, INX, BRK]);
         cpu.run();
 
-        assert_eq!(cpu.reg.x, 0xc1);
+        assert_eq!(cpu.reg.x, 0xC1);
     }
 
     #[test]
     fn test_inx_overflow() {
         let mut cpu = CPU::new();
         cpu.load(0, &[INX, INX, BRK]);
-        cpu.reg.x = 0xff;
+        cpu.reg.x = 0xFF;
         cpu.run();
 
-        assert_eq!(cpu.reg.x, 1u8);
+        assert_eq!(cpu.reg.x, 0x01);
+    }
+
+    #[test]
+    fn test_program_load() {
+        let mut cpu = CPU::new();
+        cpu.load_program(&[LDA, 0xC0, TAX, INX, BRK]);
+        cpu.interrupt_reset();
+        cpu.run();
+
+        assert_eq!(cpu.reg.x, 0xC1);
     }
 }
 
